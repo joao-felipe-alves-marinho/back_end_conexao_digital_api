e75ca58610cea7e1371f0fffe2f782c77d13a632
João Felipe Alves Marinho <83378488+joao-felipe-alves-marinho@users.noreply.github.com>
2024-09-28 17:14:39 -0300

    Update settings.py

--- conexao_digital/settings.py
+++ conexao_digital/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
 ]
 
 # CORS setup
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS') or [
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',') or [
     'http://localhost:5173',  # Allow your frontend to make requests to the backend
 ]
 
